ea45ad4d8312898b8a373cce352487ae0092fc01
jorge <jorgebertolo@gmail.com>
2020-04-14 20:25:35 -0400

    Configurado Upload de Imagens

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -1,9 +1,8 @@
-const {
-    formatPrice
-} = require('../../lib/utils')
+const { formatPrice } = require('../../lib/utils')
 
 const Category = require('../models/Category')
 const Product = require('../models/Product')
+const File = require('../models/File')
 
 module.exports = {
     create(req, res) {
@@ -31,10 +30,16 @@ module.exports = {
                 return res.send(`Todos os campos devem ser preenchidos. Campo ${key} está vazio`)
         }
 
+        if (req.files.length == 0)
+            return res.send('Por favor, envie pelo menos uma imagem.')
+
         let results = await Product.create(req.body)
         const productId = results.rows[0].id
 
-        return res.redirect(`products/${productId}`)
+        const filesPromise = req.files.map(file => File.create({ ...file, product_id: productId }))
+        await Promise.all(filesPromise)
+
+        return res.redirect(`/products/${productId}/edit`)
     },
     async edit(req, res) {
         //let result é utilizado duas vezes, por isso é let, ela é sobrescrita
@@ -46,22 +51,47 @@ module.exports = {
         product.old_price = formatPrice(product.old_price)
         product.price = formatPrice(product.price)
 
+        //get categories
         results = await Category.all()
         const categories = results.rows
 
-        return res.render(`products/edit.njk`, {
-            product,
-            categories
-        })
+        //get images
+        results = await Product.files(product.id) //error
+        let files = results.rows
+        files = files.map(file => ({
+            ...file,
+            src: `${req.protocol}://${req.headers.host}${file.path.replace('public', "")}`
+        }))
+
+        return res.render(`products/edit.njk`, { product, categories, files })
     },
     async put(req, res) {
         const keys = Object.keys(req.body)
 
         //valida se os campos estão vazios
         for (let key of keys) {
-            if (req.body[key] == "")
+            if (req.body[key] == "" && key != "removed_files")
                 return res.send(`Todos os campos devem ser preenchidos. Campo ${key} está vazio`)
         }
+
+        if (req.files.length != 0) {
+            const newFilesPromise = req.files.map(file =>
+                File.create({ ...file, product_id: req.body.id }))
+
+            await Promise.all(newFilesPromise)
+        }
+
+        if (req.body.removed_files) {
+            //1, 2, 3, 
+            const removedFiles = req.body.removed_files.split(",") //[1, 2, 3, ]
+            const lastIndex = removedFiles.length - 1
+            removedFiles.splice(lastIndex, 1) // [1, 2, 3]
+
+            const removedFilesPromise = removedFiles.map(id => File.delete(id))
+
+            await Promise.all(removedFilesPromise)
+        }
+
 
         req.body.price = req.body.price.replace(/\D/g, "")
 
